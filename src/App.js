8229cf3342215bd1b7b2c646e4b053028fe413ac
KalamAnsari <kalamansari@resiliencesoft.com>
2023-01-20 18:07:20 +0530

    --

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React,{useState} from 'react';
 import AddUser from './components/Users/AddUser';
 import UserList from './components/Users/UserList';
 
+//
 
 function App() {
-
+   
   const [userList, setuserList] = useState([]);
 
   const getUserValue=(newUserDetails)=>{
